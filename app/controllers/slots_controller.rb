523f91da16b801a32ef994682582fed421d4389d
Raghu Betina <raghu@firstdraft.com>
2018-11-30 00:56:52 +0000

    Only slot user can access show page

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,5 +1,5 @@
 class SlotsController < ApplicationController
-  before_action :current_user_must_be_slot_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_slot_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_slot_user
     slot = Slot.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
