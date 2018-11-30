eef6174adb13303cd9758c96d7981182aae86cac
Raghu Betina <raghu@firstdraft.com>
2018-11-30 01:41:59 +0000

    2nd draft

--- db/migrate/20181130013917_create_slots.rb
+++ db/migrate/20181130013917_create_slots.rb
@@ -3,8 +3,8 @@ class CreateSlots < ActiveRecord::Migration[5.1]
     create_table :slots do |t|
       t.integer :user_id
       t.date :date
-      t.time :start
-      t.time :end
+      t.time :start_time
+      t.time :end_time
 
       t.timestamps
     end
